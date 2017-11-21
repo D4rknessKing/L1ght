e2c84e925cf199ede3f69db4c7e12d5484e118aa
D4rknessKing <d4rknessking@gmail.com>
2017-11-21 19:40:10 -0300

    Forgot to include this in the last commit... Ooops

---
+++ src/main/java/br/net/brjdevs/d4rk/l1ght/utils/Stats.java
@@ -0,0 +1,15 @@
+package br.net.brjdevs.d4rk.l1ght.utils;
+
+public class Stats {
+
+    public static long loginTime = 0;
+
+    public static int readMessages = 0;
+    public static int sendMessages = 0;
+
+    public static int redditMatches = 0;
+    public static int twitterMatches = 0;
+
+    public static int executedCommands = 0;
+
+}
